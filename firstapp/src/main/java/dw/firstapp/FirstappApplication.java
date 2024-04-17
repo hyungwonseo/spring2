e744f66215c2bc4e4ae00c838c8db6c5a90b6ff0
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-04-17 17:29:22 +0900

    update

--- firstapp/src/main/java/dw/firstapp/FirstappApplication.java
+++ firstapp/src/main/java/dw/firstapp/FirstappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstappApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstappApplication.class, args);
 	}
-
 }
