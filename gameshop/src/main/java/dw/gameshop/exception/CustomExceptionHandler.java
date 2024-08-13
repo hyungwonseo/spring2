9b2ae589dbae696781a962d8e13fa58149bdd539
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2024-08-13 14:58:26 +0900

    Update CustomExceptionHandler.java

--- gameshop/src/main/java/dw/gameshop/exception/CustomExceptionHandler.java
+++ gameshop/src/main/java/dw/gameshop/exception/CustomExceptionHandler.java
@@ -8,14 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<BaseResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex) {
